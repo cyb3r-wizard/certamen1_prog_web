41455a1e44d572e2c3c6c0a90cf6809e092d357d
CodeSandbox <support@codesandbox.io>
2025-04-04 16:06:18 +0000

    Sandbox snapshot commit

--- src/routers/user.routers.js
+++ src/routers/user.routers.js
@@ -1,10 +1,19 @@
 import { Router } from "express";
-import scrypt, { randomBytes } from "node:crypto";
+import crypto, { randomBytes } from "node:crypto";
 import database from "../../database.json" assert { type: "json" };
 const userRouter = Router();
 const users = database.users;
 
-let salt16 = randomBytes(16);
+let salt16 = randomBytes(16).toString("hex");
+
+async function hashPsw(password) {
+  return new Promise((resolve, reject) => {
+    crypto.scrypt(password, salt16, 64, (err, derivedKey) => {
+      if (err) reject(err);
+      resolve(`${salt16}:${derivedKey.toString("hex")}`);
+    });
+  });
+}
 
 userRouter.get("/", (req, res) => {
   users.map((user) => {
@@ -13,19 +22,22 @@ userRouter.get("/", (req, res) => {
   res.json({ message: "ESTE SERÁ EL LOGIN " + salt16.toString("hex") });
 });
 
-userRouter.post("/", (req, res) => {
+userRouter.post("/", async (req, res) => {
   try {
     const { username, password } = req.body;
-    //hashear password antes de consultar.
+    const hidden = await hashPsw(password);
+
     let isPsw = users.find(
-      (user) => user.password === password && user.username === username
+      (user) => /*user.password === password && */ user.username === username
     );
     if (!isPsw) {
       res.status(403).json({ message: "Credenciales inválidas" });
     }
+    isPsw.password = hidden;
     console.log(isPsw);
+    console.log(users);
     res.json({
-      message: "Sesión iniciada, " + isPsw.username + salt16.toString("hex"),
+      message: `NAME ${isPsw.name}, USERNAME: ${isPsw.username}`,
     });
   } catch (err) {
     res.status(403).json({ message: "" + err });
